896172d9b62d89968eed94da07763452640c62a4
TaranAleksandr <108215188+TaranAleksandr@users.noreply.github.com>
2022-11-30 14:17:13 +0300

    Добаботан аккордион
    При запуске страницы теперь все характеристики скрыты. Открытые характеристики австоматически сворачиваются, при раскрытии других.

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,20 +1,35 @@
 const accordion = () => {
   const chItems = document.querySelectorAll(".characteristics__item");
+  const chButtons = document.querySelectorAll('.characteristics__title')
+  const chContents = document.querySelectorAll('.characteristics__description')
+
+  const inActiveAll = () => {
+    chButtons.forEach((btn) => {
+      btn.classList.remove('active')
+    })
+    chContents.forEach((cnt) => {
+      cnt.classList.remove('active')
+      cnt.style.height = ''
+    })
+  }
+
+  inActiveAll()
 
   chItems.forEach((item) => {
     const chButton = item.querySelector(".characteristics__title");
-    const chButtons = item.querySelectorAll(".characteristics__title");
     const chContent = item.querySelector(".characteristics__description");
-    const chContents = item.querySelectorAll(".characteristics__description");
 
     chButton.addEventListener("click", () => {
-      if (chContent.classList.contains("open")) {
+      inActiveAll()
+
+
+      if (chContent.classList.contains("active")) {
         chContent.style.height = "";
       } else {
         chContent.style.height = chContent.scrollHeight + "px";
       }
       chButton.classList.toggle("active");
-      chContent.classList.toggle("open");
+      chContent.classList.toggle("active");
     });
   });
 };
